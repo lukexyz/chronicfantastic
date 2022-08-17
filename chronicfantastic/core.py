7a0bddfaceca581629118c3cf6c0ed3c5194f392
lukexyz <lukedwoods@gmail.com>
2022-08-17 11:28:57 +0100

    add session password

--- chronicfantastic/core.py
+++ chronicfantastic/core.py
@@ -5,12 +5,13 @@ __all__ = ['get_joiner_url', 'get_base_url']
 
 # %% ../nbs/00_core.ipynb 1
 from IPython.display import display, Markdown
+from random import randint
 
 # %% ../nbs/00_core.ipynb 7
-def get_joiner_url(roomname, username):
+def get_joiner_url(roomname, username, password):
     # new users use this link
-    return f'https://vdo.ninja/?room={roomname}&broadcast&l={username}&m&ad&sl&view'
+    return f'https://vdo.ninja/?room={roomname}&password={password}&broadcast&l={username}&m&ad&sl&view'
 
-def get_base_url(roomname):
+def get_base_url(roomname, password):
     # director initiates room with this link
-    return f'https://vdo.ninja/?director={roomname}'
+    return f'https://vdo.ninja/?director={roomname}&password={password}'
